570eaf60676c0b54cc2cc6986175a877c4b8b0a3
GustavoMedeiros <digital.gustavomedeiros@gmail.com>
2025-04-10 14:26:49 -0300

    Criado a função verifiedIntInput() na classe VerifiedInput

--- Diamond.ConsoleApp/VerifiedInput.cs
+++ Diamond.ConsoleApp/VerifiedInput.cs
@@ -14,5 +14,30 @@ namespace Diamond.ConsoleApp
             Console.Write(message);
             return Console.ReadLine();
         }
+
+        public int verifiedIntInput(string message)
+        {
+            int returnedInputInt;
+
+            while (true)
+            {
+                string inputStr = createInput(message);
+
+                if (int.TryParse(inputStr, out int inputInt))
+                {
+                    returnedInputInt = inputInt;
+                    break;
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("\n Erro! Foi digitado letras ou múltiplos números.");
+                    Console.WriteLine(" Aperte Enter para continuar...");
+                    Console.ReadLine();
+                }
+            }
+
+            return returnedInputInt;
+        }
     }
 }
