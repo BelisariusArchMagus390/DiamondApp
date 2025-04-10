7a41fa5b5110bb7e24136f05fcdb32eb02f0d749
GustavoMedeiros <digital.gustavomedeiros@gmail.com>
2025-04-10 11:54:33 -0300

    Cálculo da linha média do diamante

--- Diamond.ConsoleApp/Program.cs
+++ Diamond.ConsoleApp/Program.cs
@@ -4,8 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.Write("Digite um número ímpara para criar um diamante: ");
+            Console.Write("Digite um número ímpar para criar um diamante: ");
             int numberDiamond = int.Parse(Console.ReadLine());
+
+            int contFilledSpaces = 1;
+
+            // Calcula a linha do meio do diamente
+            decimal temporaryValue = numberDiamond;
+            decimal midDiamondLine = Math.Ceiling(temporaryValue / 2);
+
+            
+
+            Console.ReadLine();
         }
     }
 }
