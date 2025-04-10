ace2b0c9fece59551da45357e1b8441a56702e76
GustavoMedeiros <digital.gustavomedeiros@gmail.com>
2025-04-10 16:22:35 -0300

    Criado um menu de escolhas em loop

--- Diamond.ConsoleApp/Program.cs
+++ Diamond.ConsoleApp/Program.cs
@@ -4,17 +4,64 @@
     {
         static void Main(string[] args)
         {
+            // criação de objetos
             VerifiedInput vi = new VerifiedInput();
             Diamond d = new Diamond();
 
-            string message = " Digite um número ímpar para criar um diamante: ";
-            int numberDiamond = vi.verifiedOddIntInput(message);
+            int sizeDiamond = 0;
+            string message = "";
+            bool optionExit = false;
 
-            d.createDiamond(numberDiamond);
+            while(optionExit == false)
+            {
+                Console.Clear();
+                Console.WriteLine("\n 1 - Criar diamante");
+                Console.WriteLine(" 2 - Mostrar diamante");
+                Console.WriteLine(" 3 - Sair");
 
-            d.showDiamond();
+                Console.Write("\n Escolha uma das opções: ");
+                char optionMenu = Console.ReadLine()[0];
 
-            Console.ReadLine();
+                switch (optionMenu)
+                {
+                    case '1':
+                        Console.Clear();
+                        // cria o diamante
+                        d.createDiamond(sizeDiamond);                        
+
+                        message = " Digite um número ímpar para criar um diamante: ";
+                        sizeDiamond = vi.verifiedOddIntInput(message);
+                        break;
+
+                    case '2':
+                        Console.Clear();
+
+                        if (sizeDiamond != 0)
+                        {
+                            // mostra o diamante
+                            d.showDiamond();
+                        }
+                        else
+                        {
+                            Console.WriteLine($"\n Erro! O tamanho do diamante não foi especificado.");
+                        }
+
+                        Console.WriteLine(" Aperte Enter para continuar...");
+                        Console.ReadLine();
+                        break;
+
+                    case '3':
+                        optionExit = true;
+                        break;
+
+                    default:
+                        Console.Clear();
+                        Console.WriteLine(" Erro! Opção incorreta.");
+                        Console.WriteLine(" Aperte Enter para continuar...");
+                        Console.ReadLine();
+                        break;
+                }
+            }
         }
     }
 }
