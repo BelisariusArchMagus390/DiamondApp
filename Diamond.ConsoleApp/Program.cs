08bceeb7f4167a9b0d93e706dcef66a6473e8f01
GustavoMedeiros <digital.gustavomedeiros@gmail.com>
2025-04-10 11:56:21 -0300

    Criado variável que define a quantidade de espaços brancos

--- Diamond.ConsoleApp/Program.cs
+++ Diamond.ConsoleApp/Program.cs
@@ -13,7 +13,11 @@
             decimal temporaryValue = numberDiamond;
             decimal midDiamondLine = Math.Ceiling(temporaryValue / 2);
 
-            
+            for (int line = 1; line <= numberDiamond; line++)
+            {
+                // Define a quantidade de espaços em branco que tem antes e depois dos espaços preenchidos
+                int qtWhiteSpaces = (numberDiamond - contFilledSpaces) / 2;
+            }
 
             Console.ReadLine();
         }
