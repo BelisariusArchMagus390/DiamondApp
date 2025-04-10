cdf71c3741db04d68bcc74b579a945d743132ea2
GustavoMedeiros <digital.gustavomedeiros@gmail.com>
2025-04-10 11:59:04 -0300

    Criado o print completo do diamante

--- Diamond.ConsoleApp/Program.cs
+++ Diamond.ConsoleApp/Program.cs
@@ -17,6 +17,26 @@
             {
                 // Define a quantidade de espaços em branco que tem antes e depois dos espaços preenchidos
                 int qtWhiteSpaces = (numberDiamond - contFilledSpaces) / 2;
+
+                for (int column = 1; column <= numberDiamond; column++)
+                {
+                    if ((column <= qtWhiteSpaces) || (column > (numberDiamond - qtWhiteSpaces) && column <= numberDiamond))
+                        Console.Write(" ");
+                    else
+                        Console.Write("x");
+                }
+                // compara para saber se está no meio do diamante
+                if (line == (numberDiamond - 1))
+                    contFilledSpaces = 1;
+                else
+                {
+                    if (line >= midDiamondLine)
+                        contFilledSpaces -= 2;
+                    else
+                        contFilledSpaces += 2;
+                }
+
+                Console.WriteLine();
             }
 
             Console.ReadLine();
