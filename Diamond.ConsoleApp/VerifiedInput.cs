3354554054f3a0bb1d707104e8165ed3f87bb5f8
GustavoMedeiros <digital.gustavomedeiros@gmail.com>
2025-04-10 14:21:13 -0300

    Renomeado o nome da classe

--- Diamond.ConsoleApp/VerifiedInput.cs
+++ Diamond.ConsoleApp/VerifiedInput.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Diamond.ConsoleApp
 {
-    internal class VerifyInput
+    internal class VerifiedInput
     {
+        
     }
 }
